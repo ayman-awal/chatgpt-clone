865e29c2731abec90e76d9d49a5b1d8134dd9bec
ayman-awal <ayman.awal36@gmail.com>
2024-01-21 11:48:41 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App flex">
+      <div className="sidebar flex justify-between flex-column">
+          <div>vncvbn</div>
+          <div>Ayman Awal</div>
+      </div>
+
+      <div className="main">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
